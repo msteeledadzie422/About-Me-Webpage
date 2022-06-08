6de56b60d886e60f2dab69d5288b17696b90de39
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-06-08 00:20:18 -0600

    pushing new files to origin/main

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function yourName() {
+    let userName = prompt('Hello! Would you mind sharing your name with us? Thank you!');
+    console.log(userName);
+    return document.write("Hello " + userName + ", welcome! On this page, you will be able to learn a bit more about Code Fellows Code 201 Student Mandela Steele-Dadzie!");
+}
+
