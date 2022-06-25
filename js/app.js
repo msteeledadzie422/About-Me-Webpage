a863a83d843cfa449e37dd4d3ba3735305800f1c
msteeledadzie422 <106066031+msteeledadzie422@users.noreply.github.com>
2022-06-25 15:57:47 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let userName = prompt('Hello! Would you mind sharing your name with us? Thank you!');
-console.log(userName);
+    console.log(userName);
 
 function yourName() {
     alert('Welcome, ' + userName + ', we hope you enjoy our About Me Page!')
@@ -9,52 +9,83 @@ function yourName() {
 }
 
 function questionTree() {
-    let question1 = confirm('Were you born between the years 1880 and 2022?');
-    let message = "";
-
-    if(question1 == true) {
-        let dinoRad = confirm('Amazing! Glad to know you are alive, ' + userName + '. Do you think Dinosaurs are rad?');
-
-        if(dinoRad == true) {
-            let director = confirm('We also think Dinosaurs are rad, ' + userName + ' who is alive! Do you enjoy Steven Spielberg films?');
-
-            if(director == true) {
-                let dinoMov = confirm(userName + ' who is alive, you are really on a roll here. Does your favorite Steven S. film include Dinosaurs???!!??');
-
-                if(dinoMov == true) {
-                    let finalQuestion = confirm(userName + 'you are truly on a roll! Last question: is your Steven Spielberg film Jurassic Park, released in 1993!?!?!?');
-
-                    if(finalQuestion == true) {
-                        alert(userName + ', so glad you enjoy Jurassic Park! We think you are really going to enjoy our page 😉');
-                    }
-                    else {
-                        alert(userName + 'that is surprising because you are alive and seem to enjoy Steven Spielbergs Dinosaur films.... What are these other, cherished Dinosaur-related Steven Spielberg movies you seem to think are better than Jurassic Park released in 1993?');
-                    }
-                }
-            }
-            else {
-                message = 'Steven S. has a large film library from which to choose, understandable that your favorite Steven S. film may not include Dinosaurs 😔';
-                alert(message)
-            }
-        }
-        else {
-            prompt('No problem, ' + userName + 'Steven S. may not be to everyones directorial taste. Who is your favorite director?')
-        }
+    let i = 0;
+    
+    let question1 = prompt('Let\'s play a guessing game! Answer the following questions correctly and win a special price!!!! First Question: Is my favorite color orange?');
+    switch (question1.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Sorry, that is incorrect - much to bright for me!');
+        break;
+    case 'no':
+    case 'n':
+        alert('You are correct!!!');
+        i++;
+        break;
+    default:
+        alert('Please answer by typing "yes" or "no" into this question prompt. "y" or "n" are also acceptable!');
     }
-    else {
-        let possResponse = ('old','future')
-        prompt('Wow! Are you incredibly old or from the future?')
-        // Want to add code here that will accept only responses that includes either the word old or future /not both/ within a certain character limit
-        if(possResponse == 'old') {
-            message = 'We love that, please share your tips on leading such a long and healthy life!'
-            alert(message)
-        }
-        else {
-            message = 'Wow, how did you get here? Wormhole? We have so many questions'
-            alert(message)
-        }
-        }
+    
+    let question2 = prompt('Ok, Question 2: Was I born in the US of A?');
+    switch (question1.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Bazinga! Correct!!!');
+        i++;
+        break;
+    case 'no':
+    case 'n':     
+        alert('Sorry, that is incorrect - I was born in grand ol\' Bristol, PA! :)');
+        break;
+    default:
+        alert('Please answer by typing "yes" or "no" into this question prompt. "y" or "n" are also acceptable!');
     }
+    
+    let question3 = prompt('Question 3: Am I a fan of both classic and (some) modern Hip-Hop?');
+    switch (question1.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Well done - correct!!');
+        i++;
+        break;
+    case 'no':
+    case 'n':     
+        alert('Sorry, that is incorrect - I am indeed a fan of both classic and (some) modern Hip-Hop. Apologies if this question was confusing!');
+        break;
+    default:
+        alert('Please answer by typing "yes" or "no" into this question prompt. "y" or "n" are also acceptable!');
+    }
+    
+    let question4 = prompt('Question 4: Do I speak Spanish?');
+    switch (question1.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Sorry, that is incorrect - I speak French!!! (**I took French in High School/college and am now taking lessons as a refresher!!!)');
+        break;
+    case 'no':
+    case 'n':
+        alert('That is correct - I speak French!!! (**I took French in High School/college and am now taking lessons as a refresher!!!)');
+        i++;
+        break;    
+    default:
+        alert('Please answer by typing "yes" or "no" into this question prompt. "y" or "n" are also acceptable!');
+    }
+    
+    let question5 = prompt('Question 5: Did I grow up playing soccer as my primary sport?');
+    switch (question1.toLowerCase()) {
+    case 'yes':
+    case 'y':
+        alert('Correctomundo!!!! iva la joga bonito!!!');
+        i++;
+        break;
+    case 'no':
+    case 'n':     
+        alert('Sorry, incorrect - I grew up playing the beautiful game and love it to this day!');
+        break;
+    default:
+        alert('Please answer by typing "yes" or "no" into this question prompt. "y" or "n" are also acceptable!');
+    }
+}
 
 yourName()
 questionTree()
